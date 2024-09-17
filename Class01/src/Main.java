ecf14296e8ae28ba3a0cd99fc47dd27aeafb1c57
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-09-16 22:06:58 -0300

    Terminal test

--- Class01/src/Main.java
+++ Class01/src/Main.java
@@ -6,16 +6,27 @@ public class Main {
         Scanner sc = new Scanner(System.in); // Create a Scanenr;
         Locale.setDefault(Locale.US);
 
+        System.out.print("Enter a Integer: ");
         int inputInterger = sc.nextInt();
+
+        System.out.print("Enter a Integer Array: ");
+        System.out.println();
 
         int[] inputArray = new int[inputInterger];
 
         for (int position = 0; position < inputInterger; position++) {
             inputArray[position] = sc.nextInt();
         }
+        sc.nextLine();
+
+        System.out.println("Enter a String: ");
         String inputString = sc.nextLine();
+        System.out.println();
 
-        new LearningBasics(inputInterger,inputArray,inputString); // Create Object
+        LearningBasics learn = new  LearningBasics(inputInterger,inputArray,inputString); // Create Object
 
+        System.out.println(learn);
+        System.out.println();
+        sc.close();
     }
 }
