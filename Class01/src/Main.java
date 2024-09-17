3ac108b1f623e7a30a39d88b27154ba952228b77
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-09-16 21:44:18 -0300

    Main

--- Class01/src/Main.java
+++ Class01/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in); // Create a Scanenr;
+        Locale.setDefault(Locale.US);
+
+        int inputInterger = sc.nextInt();
+
+        int[] inputArray = new int[inputInterger];
+
+        for (int position = 0; position < inputInterger; position++) {
+            inputArray[position] = sc.nextInt();
+        }
+        String inputString = sc.nextLine();
+
+        new LearningBasics(inputInterger,inputArray,inputString); // Create a Object
+
     }
 }
