d332ea90dcf8487f69bc1f060df90d289e070480
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-09-16 21:42:34 -0300

    Pratices

--- Class01/src/LearningBasics.java
+++ Class01/src/LearningBasics.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 public class LearningBasics{
+    private int firstIntVarible; // Explicit data type and encapsulation
+    private int[] ArrayVarible; // Explicit data type and encapsulation
+    private String StringVarible; // Explicit data type and encapsulation
 
 
-    
+    LearningBasics(int firstIntVarible, int[] ArrayVarible, String StringVarible){ // This is a construtor, responsable to create to inicialize an object in Main
+        this.firstIntVarible = firstIntVarible;
+        this.ArrayVarible = ArrayVarible;
+        this.StringVarible = StringVarible;
+    }
+
+    public String thatIsAMethod(String someString){
+        return "Print: %s" + StringVarible;
+    }
+
+
+    @Override
+    public String toString() {
+        return "LearningBasics{" +
+                "firstIntVarible=" + firstIntVarible +
+                ", ArrayVarible=" + Arrays.toString(ArrayVarible) +
+                ", StringVarible='" + StringVarible + '\'' +
+                '}';
+    } // Formated Output
 }
