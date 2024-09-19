e43e84533f1f023a6e6eb3710acaf36f565e244e
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-09-18 22:01:50 -0300

    Exercise

---
+++ Class01/MaquinaDeRefrigerante/src/MaquinaDeRefrigerante.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class MaquinaDeRefrigerante{
+    private String marca;
+    private ArrayList<String> refrigerantes;
+    private ArrayList<Double> precos;
+    
+    public MaquinaDeRefrigerante(String marca){
+        this.marca = marca;
+    }
+    
+    public void setRefrigerantes(ArrayList<String> soda){
+        this.refrigerantes = soda;
+    }
+    
+    public void setPreços(ArrayList<Double> precos){
+        this.precos = precos;
+    }
+
+    @Override
+    public String toString() {
+        return "MaquinaDeRefrigerante{" +
+                "marca='" + marca + '\'' +
+                ", refrigerantes=" + refrigerantes +
+                ", precos=" + precos +
+                '}';
+    }
+}
