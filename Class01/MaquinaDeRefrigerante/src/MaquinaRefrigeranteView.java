cb7c10ed9433a60400aabb5c6e302fc013c2ed64
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-09-24 20:02:52 -0300

    Class Exercise

--- Class01/MaquinaDeRefrigerante/src/MaquinaRefrigeranteView.java
+++ Class01/MaquinaDeRefrigerante/src/MaquinaRefrigeranteView.java
@@ -1,15 +1,11 @@
-import java.util.Locale;
-import java.util.Scanner;
-
-
-public class Main {
+public class MaquinaRefrigeranteView {
     public static void main(String[] args) {
 
         MaquinaRefrigerante maquina = new MaquinaRefrigerante(3);
 
         maquina.imprimirPreco();
 
-        maquina.inserirDinheiro(5);
+        maquina.inserirDinheiro(2);
 
         System.out.println("Balanço => R$" + maquina.getBalanco() + ",00");
         System.out.println();
