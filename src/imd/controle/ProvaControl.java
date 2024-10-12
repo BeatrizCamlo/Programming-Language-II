942956b074f63d7e4a394e148747318fa9c153da
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-10-12 20:19:30 -0300

    Initial commit

---
+++ src/imd/controle/ProvaControl.java
@@ -0,0 +1,25 @@
+package imd.controle;
+
+public class ProvaControl {
+	
+	public double verificarNotaParte1(double notaParte1) {
+		if (notaParte1 <= 5 && notaParte1 >=0) {
+			return notaParte1;
+		}
+		else {
+			System.out.println("Nota parte 01 - invalida!");
+			return 0;
+		}	
+	}
+	
+	public double verificarNotaParte2(double notaParte2) {
+		if (notaParte2 <= 5 && notaParte2 >=0) {
+			return notaParte2;
+		}
+		else {
+			System.out.println("Nota parte 02 - invalida!");
+			return 0;
+		}	
+	}
+
+}
---
+++ src/imd/modelo/Turma.java
@@ -0,0 +1,32 @@
+package imd.modelo;
+
+import java.util.ArrayList;
+import imd.modelo.Aluno;
+
+public class Turma {
+	
+	private ArrayList<Aluno> alunos;
+	
+	public Turma() {
+		alunos = new ArrayList<Aluno>();
+	}
+	
+	public void inserirAluno(Aluno a)  {
+		alunos.add(a);
+		System.out.println("Aluno inserido com sucesso!!");
+	}
+	
+	public String calcularMediaTurma() {
+		String output = "";
+		output += "------------------------------------------------";
+	
+		double mediaTurma = 0;
+		for (Aluno a : alunos) {
+			mediaTurma += a.calculaMedia();
+		}
+		mediaTurma = mediaTurma / alunos.size();
+		output += "\nMedia da Turma => " + mediaTurma + "\n";
+		output += "------------------------------------------------\n";
+		return output;
+	}
+}
